ee4b752b33f8abc015462bb8d0a447c1f223a3f9
Saumyaketu123 <saumyaketu1908@gmail.com>
2026-01-21 23:31:26 +0530

    feat: Setting up Backend

---
+++ server/index.js
@@ -0,0 +1,33 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+
+dotenv.config();
+const app = express();
+
+app.use(cors());
+app.use(express.json({ limit: "30mb", extended: true }));
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
+
+app.get("/", (req, res) => {
+  res.send("Youtube's backend is working");
+});
+
+app.use(bodyParser.json());
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+const DBURL = process.env.DB_URL;
+mongoose
+  .connect(DBURL)
+  .then(() => {
+    console.log("Mongodb connected");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
